0d7266723dc5a66a28c092f2f35db68e5bbf3f4f
fcy <807715333@qq.com>
2019-08-19 01:44:53 +0800

    fix dir

---
+++ src/main/java/com/fcy/Java/DesignPattern/NullObject/AbstractCustomer.java
@@ -0,0 +1,12 @@
+package com.fcy.Java.DesignPattern.NullObject;
+
+/**
+ * @descripiton:
+ * @author: fcy
+ * @date: 2018-11-24  21:22
+ */
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract String getName();
+    public abstract boolean isNil();
+}

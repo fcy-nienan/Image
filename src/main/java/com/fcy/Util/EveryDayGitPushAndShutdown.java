b832dc6bdf0e32fbe41bd7dea1e38b36e1c13070
807715333@qq.com <807715333@qq.com>
2019-10-12 18:23:25 +0800

    't'

---
+++ src/main/java/com/fcy/Util/EveryDayGitPushAndShutdown.java
@@ -0,0 +1,14 @@
+package com.fcy.Util;
+
+import java.util.logging.Logger;
+
+public class EveryDayGitPushAndShutdown {
+    private static Logger logger = Logger.getLogger(EveryDayGitPushAndShutdown.class.getName());
+
+    public static void main(String args[]) throws Exception {
+        Process process=null;
+        try{
+            process=Runtime.getRuntime().exec("cmd ");
+        }
+    }
+}

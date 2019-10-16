ef7e45ba495532fb4803ad4eb5d30b1da8205336
807715333@qq.com <807715333@qq.com>
2019-10-16 19:18:48 +0800

    'sout'

---
+++ src/main/java/com/fcy/Util/sout.java
@@ -0,0 +1,14 @@
+package com.fcy.Util;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class sout {
+    private static Logger logger = Logger.getLogger(sout.class.getName());
+    public static void main(String args[]) throws Exception {
+
+    }
+    public static void add(String s){
+
+    }
+}

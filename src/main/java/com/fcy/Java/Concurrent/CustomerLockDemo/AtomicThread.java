cdbef13732dd5c1c5eba479146fb9d0ce109edba
fcy <807715333@qq.com>
2019-10-18 23:53:57 +0800

    '20191018235354'

---
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/AtomicThread.java
@@ -0,0 +1,7 @@
+package com.fcy.Java.Concurrent.CustomerLockDemo;
+
+public class AtomicThread {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/MESIThread.java
@@ -0,0 +1,7 @@
+package com.fcy.Java.Concurrent.CustomerLockDemo;
+
+public class MESIThread {
+    public static void main(String args[]) {
+
+    }
+}

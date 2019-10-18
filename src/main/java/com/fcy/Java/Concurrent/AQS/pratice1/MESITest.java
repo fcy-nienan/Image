e6b445d7d56a210b9f89bffba7e4177d83fe36ff
fcy <807715333@qq.com>
2019-10-18 23:04:13 +0800

    '20191018230403'

--- src/main/java/com/fcy/Java/Concurrent/AQS/pratice1/MESITest.java
+++ src/main/java/com/fcy/Java/Concurrent/AQS/pratice1/MESITest.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 //MESI协议  若缓存行处于S状态,如果此时有另一个处理器发出了Invalid信息
@@ -22,6 +23,7 @@ public class MESITest{
     private int y;
     private int z;
     private int s;
+    private AtomicInteger a=new AtomicInteger();
     private static long zOffset;
     private static Unsafe unsafe;
     private static MESITest test=new MESITest();
@@ -48,9 +50,18 @@ public class MESITest{
             s++;
         }
     }
+    public void adda(){
+        a.getAndIncrement();
+    }
+    public final void getAndAddInt(Object var1, long var2, int var4) {
+        int var5;
+        do {
+            var5 = unsafe.getIntVolatile(var1, var2);
+        } while(!unsafe.compareAndSwapInt(var1, var2, var5, var5 + var4));
+    }
     public void addz(){
+        getAndAddInt(test,zOffset,1);
 
-        update(test,zOffset);
     }
     public void addx(){
         x++;
@@ -90,6 +101,14 @@ public class MESITest{
             }
         }
     }
+    class at implements Runnable{
+        @Override
+        public void run() {
+            for (int i=0;i<10000000;i++){
+                adda();
+            }
+        }
+    }
     public static void main(String args[]) {
         test.test();
     }
@@ -102,10 +121,12 @@ public class MESITest{
 //            Future<?> submit1 = executor.submit(test.new yt());
             Future<?> submit2 = executor.submit(test.new zt());
 //            Future<?> submit3 = executor.submit(test.new st());
+//            Future<?> submit4 = executor.submit(test.new at());
 //            futures.add(submit);
 //            futures.add(submit1);
             futures.add(submit2);
 //            futures.add(submit3);
+//            futures.add(submit4);
         }
         while (true){
             int count=0;
@@ -126,6 +147,7 @@ public class MESITest{
         System.out.println("y:"+y);
         System.out.println("z:"+z);
         System.out.println("s:"+s);
+        System.out.println("a:"+a);
         executor.shutdown();
 
     }

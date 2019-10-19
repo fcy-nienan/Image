c96724dff857e84f1b04132cdebef277dd335c4e
fcy <807715333@qq.com>
2019-10-20 01:35:37 +0800

    '20191020'

--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/jniDemo.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/jniDemo.java
@@ -1,7 +1,11 @@
 package com.fcy.Java.Concurrent.CustomerLockDemo;
 
-public class jniDemo {
-    public static void main(String args[]) {
-
+public class jniDemo{
+    static{
+        System.loadLibrary("testSO");
     }
+    public int incrementX(int x){
+        return increment(x);
+    }
+    public native int increment(int x);
 }
--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/other.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/other.java
@@ -2,6 +2,21 @@ package com.fcy.Java.Concurrent.CustomerLockDemo;
 
 public class other {
     public static void main(String args[]) {
+        jniDemo demo=new jniDemo();
+        long start,end;
+        start=System.currentTimeMillis();
+        for (int i=0;i<Integer.MAX_VALUE;i++){
+            demo.increment(i);
+        }
+        end=System.currentTimeMillis();
+        System.out.println("cost time:"+(end-start));
+
+        start=System.currentTimeMillis();
+        for (int i=0;i<Integer.MAX_VALUE;i++){
+            demo.incrementX(i);
+        }
+        end=System.currentTimeMillis();
+        System.out.println("cost time:"+(end-start));
 
     }
 }

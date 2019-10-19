6f8df244269b3a2fd5b747a442ee6f8cbb83af24
fcy <807715333@qq.com>
2019-10-20 02:29:31 +0800

    '20191020'

--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestDoubleNative.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestDoubleNative.java
@@ -2,6 +2,7 @@ package com.fcy.Java.Concurrent.CustomerLockDemo;
 
 public class TestDoubleNative {
     public static void main(String args[]) {
+        Integer integer;
         long start,end;
         start = System.currentTimeMillis();
         for (long i=0;i<Integer.MAX_VALUE;i++){

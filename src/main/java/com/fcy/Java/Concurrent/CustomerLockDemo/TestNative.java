cd1678b8641ff5c25910e0f8e363e666c1711452
fcy <807715333@qq.com>
2019-10-20 16:37:58 +0800

    '20191020163756'

--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestNative.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestNative.java
@@ -6,6 +6,19 @@ import java.io.File;
 
 public class TestNative {
     public static void main(String args[]) throws InterruptedException {
+        testNative();
+        testNative();
+        testNative();
+        testNative();
+
+        testOuter();
+        testOuter();
+        testOuter();
+        testOuter();
+
+
+    }
+    private static void testNative(){
         long total1=0;
         long t1 = System.currentTimeMillis();
         for (int i = 0; i < Integer.MAX_VALUE; i++) {//直接使用本地方法
@@ -13,34 +26,8 @@ public class TestNative {
         }
         long t2 = System.currentTimeMillis();
         System.out.println(t2 - t1 + "ms");
-
-
-        long total3=0;
-        long t5 = System.currentTimeMillis();
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {//直接使用本地方法
-            total3 = Double.doubleToLongBits(i);
-        }
-        long t6 = System.currentTimeMillis();
-        System.out.println(t6 - t5 + "ms");
-
-        long ttttt=0;
-        long tttt1 = System.currentTimeMillis();
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {//直接使用本地方法
-            ttttt = Double.doubleToLongBits(i);
-        }
-        long tttt2 = System.currentTimeMillis();
-        System.out.println(tttt2 - tttt1 + "ms");
-
-
-        long total2=0;
-        long t3 = System.currentTimeMillis();
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {//外部调用本地方法
-            total2 = doubleToLongBits(i);
-        }
-        long t4 = System.currentTimeMillis();
-        System.out.println(t4 - t3 + "ms");
-
-
+    }
+    private static void testOuter(){
         long ttt=0;
         long ttt1 = System.currentTimeMillis();
         for (int i = 0; i < Integer.MAX_VALUE; i++) {//外部调用本地方法
@@ -48,14 +35,6 @@ public class TestNative {
         }
         long ttt2 = System.currentTimeMillis();
         System.out.println(ttt2 - ttt1 + "ms");
-
-
-
-
-
-
-
-        System.out.println(total1+total2);
     }
     public static long doubleToLongBits(double value) {
         long result =Double.doubleToRawLongBits(value);

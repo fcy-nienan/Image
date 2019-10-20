ed095deb645592eafaf28c0b8d001e161e08f100
fcy <807715333@qq.com>
2019-10-20 13:12:16 +0800

    '20191020131213'

--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/AB.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/AB.java
@@ -1,7 +1,26 @@
 package com.fcy.Java.Concurrent.CustomerLockDemo;
-
 public class AB {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            Thread.enumerate(new Thread[]{Thread.currentThread()});
+        }
+
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1 + "ms");
+
+        long t3 = System.currentTimeMillis();
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            enumerate(new Thread[]{Thread.currentThread()});
+        }
+        long t4 = System.currentTimeMillis();
+        System.out.println(t4 - t3 + "ms");
 
     }
+
+    public static int enumerate(Thread tarray[]) {
+        return Thread.currentThread().getThreadGroup().enumerate(tarray);
+    }
 }
+

161fb18225a9283fd296a90473c055375dd449c3
fcy <807715333@qq.com>
2019-10-20 12:49:09 +0800

    '20191020124906'

--- src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestNative.java
+++ src/main/java/com/fcy/Java/Concurrent/CustomerLockDemo/TestNative.java
@@ -6,12 +6,6 @@ import java.io.File;
 
 public class TestNative {
     public static void main(String args[]) throws InterruptedException {
-        Object o=new Object();
-        Thread.currentThread();
-        long start=System.currentTimeMillis();
-
-
-
         long total1=0;
         long t1 = System.currentTimeMillis();
         for (int i = 0; i < Integer.MAX_VALUE; i++) {

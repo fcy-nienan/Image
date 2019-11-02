2f8b5703c5bfbd9d3958d6b890a1aecd04790f7a
fcy <807715333@qq.com>
2019-11-03 03:05:49 +0800

    '20191103'

--- Java/src/main/java/Reference/PhantomReferenceDemo.java
+++ Java/src/main/java/Reference/PhantomReferenceDemo.java
@@ -1,5 +1,6 @@
 package Reference;
 
+import ThreadUtil.TMS;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.ref.PhantomReference;
@@ -7,21 +8,21 @@ import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 @Slf4j
 public class PhantomReferenceDemo {
-    public static void main (String args[]) {
+    public static void main (String args[]) throws InterruptedException {
         execute();
     }
-    public static void execute(){
+    public static void execute() throws InterruptedException {
         ReferencedObject object=new ReferencedObject();
         ReferenceQueue queue=new ReferenceQueue();
         PhantomReference phantomReference=new PhantomReference(object,queue);
-        System.out.println(queue.poll());
-        while (true){
-            System.out.println(phantomReference.isEnqueued());
-            log.info(String.valueOf(phantomReference.enqueue()));
-            if (phantomReference.get()==null){
-                log.info("object has been collected!");
-                break;
-            }
-        }
+        System.out.println(phantomReference==null);
+        System.out.println(phantomReference.get() == null);
+        checkReferenceQueueThread thread=new checkReferenceQueueThread(queue);
+        thread.start();
+        System.out.println(phantomReference==null);
+        System.out.println(phantomReference.get() == null);
+        TMS.sleep(5000);
+        System.gc();
+
     }
 }

b3179887813431e28f6c7ff8d4c7b26feee94c72
fcy <807715333@qq.com>
2019-11-04 06:00:30 +0800

    '20191104'

--- Experiment/src/main/java/ClearBuilderTest/ClearBuilder.java
+++ Experiment/src/main/java/ClearBuilderTest/ClearBuilder.java
@@ -9,7 +9,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-
+//测试结果相差不大,因为两者底层都是通过System.arrayCopy()方法实现的
 public class ClearBuilder {
     public static void main (String args[]) throws RunnerException {
         Options options= new OptionsBuilder()
@@ -22,7 +22,7 @@ public class ClearBuilder {
     @BenchmarkMode(Mode.AverageTime)
     public static void remove(){
         StringBuilder builder=new StringBuilder();
-        for (int i=0;i<100000;i++){
+        for (int i=0;i<10000;i++){
             builder.append(StringUtil.getRandomString(10,true));
         }
         builder.delete(0,builder.length());
@@ -31,9 +31,18 @@ public class ClearBuilder {
     @BenchmarkMode(Mode.AverageTime)
     public static void setLength(){
         StringBuilder builder=new StringBuilder();
-        for (int i=0;i<100000;i++){
+        for (int i=0;i<10000;i++){
             builder.append(StringUtil.getRandomString(10,true));
         }
         builder.setLength(0);
+    }
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public static void newObject(){
+        StringBuilder builder=new StringBuilder();
+        for (int i=0;i<10000;i++){
+            builder.append(StringUtil.getRandomString(10,true));
+        }
+        builder=new StringBuilder();
     }
 }

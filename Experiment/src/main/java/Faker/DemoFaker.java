5811486bf965fa26cd7d2e8e9e46f0c651618460
fcy <807715333@qq.com>
2019-11-11 01:27:21 +0800

    '20191111'

--- Experiment/src/main/java/Faker/DemoFaker.java
+++ Experiment/src/main/java/Faker/DemoFaker.java
@@ -5,17 +5,24 @@ import com.github.javafaker.Faker;
 import java.util.Locale;
 
 public class DemoFaker {
+    static class hashDemo{
+        private String t;
+        private int tt;
+    }
     public static void main (String args[]) {
-        Faker faker = new Faker(Locale.CHINA);
-
-        String name = faker.name().fullName(); // Miss Samanta Schmidt
-        String firstName = faker.name().firstName(); // Emory
-        String lastName = faker.name().lastName(); // Barton
-
-        String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
-        System.out.println(firstName);
-        System.out.println(lastName);
-        System.out.println(streetAddress);
-        System.out.println(name);
+        Object o=new Object();
+        System.out.println(o.hashCode());
+        System.out.println(new hashDemo().hashCode());
+//        Faker faker = new Faker(Locale.CHINA);
+//
+//        String name = faker.name().fullName(); // Miss Samanta Schmidt
+//        String firstName = faker.name().firstName(); // Emory
+//        String lastName = faker.name().lastName(); // Barton
+//
+//        String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+//        System.out.println(firstName);
+//        System.out.println(lastName);
+//        System.out.println(streetAddress);
+//        System.out.println(name);
     }
 }

51cfbd7a21e47be40b0641ddf2c1c6c99417670e
fcy <807715333@qq.com>
2019-11-14 03:22:23 +0800

    '20191114'

--- Util/src/main/java/Attack/DBSource.java
+++ Util/src/main/java/Attack/DBSource.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
-@Data
-@NoArgsConstructor
 @ToString
 @Slf4j
 public class DBSource implements CommandSource {
@@ -23,6 +21,12 @@ public class DBSource implements CommandSource {
     private DBConnectionUtil connectionUtil=new DBConnectionUtil();
     public DBSource(){
         this(System.getProperty("user.home")+ File.separator+"db.ini");
+    }
+
+    public static void main (String[] args) {
+        for (String command : new DBSource("D:\\command").getCommands()) {
+            System.out.println(command);
+        }
     }
     public DBSource(String path){
         this.path=path;
@@ -34,28 +38,41 @@ public class DBSource implements CommandSource {
     }
     @Override
     public List<String> getCommands () {
-        return null;
+        return selectCommandsAndUpdateCount();
     }
-
-    public static void main (String[] args) {
-
-    }
-    private List<String> selectCommands(){
+    private List<String> selectCommandsAndUpdateCount(){
         List<String> commandList=new ArrayList<>();
+        StringBuilder builder=new StringBuilder();
         Connection connection = connectionUtil.getConnection();
         PreparedStatement preparedStatement =null;
         ResultSet resultSet = null;
         try {
-            preparedStatement= connection.prepareStatement("select * from command");
+            preparedStatement= connection.prepareStatement("select * from command where count=0");
             resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
                 int id = resultSet.getInt("id");
-                String command = resultSet.getString("command");
+                builder.append(id).append(",");
+                String command = resultSet.getString("name");
                 commandList.add(command);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        if (builder.length()!=0) {
+            builder.deleteCharAt(builder.length() - 1);
+            updateCount(builder.toString());
+        }
         return commandList;
+    }
+    private void updateCount(String ids){
+        Connection connection=connectionUtil.getConnection();
+        PreparedStatement preparedStatement;
+        try{
+            preparedStatement=connection.prepareStatement("update command set count=count+1 where id in(?)");
+            preparedStatement.setString(1,ids);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }

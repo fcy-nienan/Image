62f8a6df6c8d08ab1f514632952b176148d607ed
fcy <807715333@qq.com>
2019-11-15 09:30:42 +0800

    '20191115'

--- Java/src/main/java/Advanced/Concurrent/Thread/FutureTaskDemo.java
+++ Java/src/main/java/Advanced/Concurrent/Thread/FutureTaskDemo.java
@@ -1,7 +1,19 @@
 package Advanced.Concurrent.Thread;
 
+import ThreadUtil.TP;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+/*
+* 未来对象模式
+*
+* */
 public class FutureTaskDemo {
     public static void main (String args[]) {
+        ThreadPoolExecutor tp= TP.getTPE();
+        Future<?> submit = tp.submit(() -> {
+
+        });
 
     }
 }

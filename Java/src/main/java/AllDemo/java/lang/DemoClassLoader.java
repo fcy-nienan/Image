11bec6b86191e294d8a45e514f884ef45ea58a3e
fcy <807715333@qq.com>
2019-11-15 22:17:49 +0800

    '20191115221744'

--- Java/src/main/java/AllDemo/java/lang/DemoClassLoader.java
+++ Java/src/main/java/AllDemo/java/lang/DemoClassLoader.java
@@ -4,11 +4,12 @@ import sun.misc.Launcher;
 import sun.misc.URLClassPath;
 
 import java.net.URL;
+import java.net.URLClassLoader;
 
 public class DemoClassLoader {
-    public static void main (String args[]) {
+    public static void main (String args[]) throws InterruptedException {
         disPath();
-        ClassLoader loader;
+        Thread.sleep(3000000);
     }
     public static void disPath(){
         System.out.println("----------bootstrap class loader path----------");
@@ -19,20 +20,62 @@ public class DemoClassLoader {
         }
 
         System.out.println("----------extension class loader path----------");
-        String paths=System.getProperty("java.ext.dirs");
-        String[] path=paths.split(";");
-        for (String s : path) {
-            System.out.println(s);
+        URLClassLoader parent = (URLClassLoader) (ClassLoader.getSystemClassLoader().getParent());
+        URL[] urLs1 = parent.getURLs();
+        for (URL url : urLs1) {
+            System.out.println(url.getPath());
         }
 
         System.out.println("----------system class loader path----------");
-        String appPaths=System.getProperty("java.class.path");
-        String[] appPath=appPaths.split(";");
-        for (String s : appPath) {
-            System.out.println(s);
+        URLClassLoader systemClassLoader = (URLClassLoader) (ClassLoader.getSystemClassLoader());
+        URL[] urLs2 = systemClassLoader.getURLs();
+        for (URL url : urLs2) {
+            System.out.println(url.getPath());
         }
 
-
-
+        String paths=System.getProperty("java.ext.dirs");
+        String appPaths=System.getProperty("java.class.path");
     }
+    /*
+    *
+    *
+    *               正常的三个类加载器的路径,IDE中的一些都是额外配置了的
+         ----------bootstrap class loader path----------
+        /D:/Code/Language/java/jre/lib/resources.jar
+        /D:/Code/Language/java/jre/lib/rt.jar
+        /D:/Code/Language/java/jre/lib/sunrsasign.jar
+        /D:/Code/Language/java/jre/lib/jsse.jar
+        /D:/Code/Language/java/jre/lib/jce.jar
+        /D:/Code/Language/java/jre/lib/charsets.jar
+        /D:/Code/Language/java/jre/lib/jfr.jar
+        /D:/Code/Language/java/jre/classes
+        ----------extension class loader path----------
+        /D:/Code/Language/java/jre/lib/ext/access-bridge-64.jar
+        /D:/Code/Language/java/jre/lib/ext/cldrdata.jar
+        /D:/Code/Language/java/jre/lib/ext/dnsns.jar
+        /D:/Code/Language/java/jre/lib/ext/jaccess.jar
+        /D:/Code/Language/java/jre/lib/ext/jfxrt.jar
+        /D:/Code/Language/java/jre/lib/ext/localedata.jar
+        /D:/Code/Language/java/jre/lib/ext/nashorn.jar
+        /D:/Code/Language/java/jre/lib/ext/sunec.jar
+        /D:/Code/Language/java/jre/lib/ext/sunjce_provider.jar
+        /D:/Code/Language/java/jre/lib/ext/sunmscapi.jar
+        /D:/Code/Language/java/jre/lib/ext/sunpkcs11.jar
+        /D:/Code/Language/java/jre/lib/ext/zipfs.jar
+        ----------system class loader path----------
+        /D:/
+        /D:/Code/Language/java/lib/dt.jar
+        /D:/Code/Language/java/lib/tools.jar
+        /D:/Code/Language/java/jre/lib/rt.jar
+
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
 }

9af15261b4e7a320a1fdda621f14406e8f2a06a4
807715333@qq.com <807715333@qq.com>
2019-11-19 18:13:13 +0800

    'commandAttack'

--- Java/src/main/java/AllDemo/java/io/DemoMMap.java
+++ Java/src/main/java/AllDemo/java/io/DemoMMap.java
@@ -2,6 +2,7 @@ package AllDemo.java.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -15,6 +16,7 @@ public class DemoMMap {
         File file=new File("D:\\command");
         FileInputStream fileInputStream=new FileInputStream(file);
         FileChannel channel = fileInputStream.getChannel();
+        RandomAccessFile accessFile=new RandomAccessFile("D:\\","rw");
         MappedByteBuffer map = channel.map(MapMode.READ_ONLY, 0, 1000);
         byte[] bytes=new byte[1000];
         ByteBuffer buffer = map.get(bytes);

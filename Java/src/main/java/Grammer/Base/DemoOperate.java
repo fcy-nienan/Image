a0ba03f4f40568218dc26d0dfbae22524e14403a
fcy <807715333@qq.com>
2019-11-20 06:32:44 +0800

    '20191120'

---
+++ Java/src/main/java/Grammer/Base/DemoOperate.java
@@ -0,0 +1,22 @@
+package Grammer.Base;
+
+import java.util.BitSet;
+
+/*
+* &     逻辑与
+* |     逻辑或
+* !     逻辑非
+* ^     异或
+* ~     取反
+* <<    左移
+* >>    右移
+* >>>   无符号右移
+*
+*
+* */
+public class DemoOperate {
+    public static void main (String args[]) {
+        BitSet set=new BitSet();
+
+    }
+}

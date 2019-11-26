ebc443bc66ded98ef22dd8b5dc30311b880d757e
fcy <807715333@qq.com>
2019-11-26 22:01:05 +0800

    '20191126220101'

--- Algorithm/src/main/java/LeetCode/Array/twoSum.scala
+++ Algorithm/src/main/java/LeetCode/Array/twoSum.scala
@@ -2,19 +2,18 @@ package LeetCode.Array
 
 object twoSum {
   def twoSum(nums:Array[Int],target:Int):Array[Int]={
-    val result:List[Int]=List()
+    var result:List[Int]=List()
     var map: Map[Int, Int] = Map()
     var i=0;
     for(i <- 0 until nums.length){
       var reduce=target-nums(i)
       var index=map.get(reduce)
-
       if (map.contains(reduce)){
-        result.appended(map.get(reduce))
-        result
-        result.appended(nums(i))
-        map += (nums(i)->i)
+        result=result.appended(map.get(reduce))
+        result=result.appended(nums(i))
+        return result.toArray
       }
+      map+=(nums(i)->i)
     }
 
     result.toArray

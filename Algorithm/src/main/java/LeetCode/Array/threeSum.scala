8bbf50018cbc7508393e1ec0b90985e4049ce28c
fcy <807715333@qq.com>
2019-12-02 00:04:22 +0800

    '20191202'

--- Algorithm/src/main/java/LeetCode/Array/threeSum.scala
+++ Algorithm/src/main/java/LeetCode/Array/threeSum.scala
@@ -1,19 +1,30 @@
 package LeetCode.Array
 
 object threeSum {
-  def threeSum(nums: Array[Int]): List[List[Int]] = {
-    for (i <- 0 until nums.length){
-      
-    }
+  def main(args: Array[String]): Unit = {
+    var x=Array(-1,0,1,2,-1,-4)
+    var t=threeSum(x)
+    println(t)
   }
-  def twoSum(nums:Array[Int],target:Int):Array[Int]={
-    var map:Map[Int,Int]=Map()
-    for (i <- 0 until nums.length){
-      if (map.contains(target-nums(i))&&map(target-nums(i))!=i){
-        return Array(nums(i),target-nums(i))
-      }
-      map+=nums(i)->i
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+    var map:Map[Int,String]=Map()
+    for (i <- 0 until nums.length;j<- i+1 until nums.length){
+      var t=0-nums(i)-nums(j)
+      map+=t->(i+","+j)
     }
-    return null
+    var t:List[List[Int]]=List()
+    for(i<-0 until nums.length){
+      if (map.contains(nums(i))&&map(nums(i)).indexOf(i+"")==(-1)){
+        var x:List[Int]=List()
+        x=x.::(nums(i))
+        val strings = map(nums(i)).split(",")
+        for(j<-strings){
+          x=x.::(nums(j.toInt))
+        }
+        t=t.::(x)
+        map=map.removed(nums(i))
+      }
+    }
+    t
   }
 }

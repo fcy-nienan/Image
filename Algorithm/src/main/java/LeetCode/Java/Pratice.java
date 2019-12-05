9ef3eb9fd87fafe8587b7ffc63e1432ed040286f
fcy <807715333@qq.com>
2019-12-06 01:07:31 +0800

    '20191206'

---
+++ Algorithm/src/main/java/LeetCode/Java/Pratice.java
@@ -0,0 +1,7 @@
+package LeetCode.Java;
+
+public class Pratice {
+    public static void main (String args[]) {
+
+    }
+}
--- Algorithm/src/main/java/LeetCode/Recursion/longestUnivaluePath.scala
+++ Algorithm/src/main/java/LeetCode/Recursion/longestUnivaluePath.scala
@@ -1,4 +1,9 @@
 package LeetCode.Recursion
+
+import java.util
+
+import scala.collection.mutable
+
 class TreeNode(var _value: Int) {
      var value: Int = _value
      var left: TreeNode = null
@@ -6,6 +11,20 @@ class TreeNode(var _value: Int) {
    }
 object longestUnivaluePath {
   def longestUnivaluePath(root: TreeNode): Int = {
+    if (root!=null){
+      var sum=0;
+      if (root!=null&&root.left!=null&&root.value==root.left.value) {
+        sum += 1;
+        sum+=longestUnivaluePath(root.left)
+      }
+      if (root!=null&&root.right!=null&&root.value==root.right.value){
+        sum+=1;
+        sum+=longestUnivaluePath(root.right)
+      }
+      sum
+    }
+  }
+  def preOrder(root:TreeNode):Unit={
 
   }
 }

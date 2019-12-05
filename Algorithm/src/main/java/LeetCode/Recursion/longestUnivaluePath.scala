59f7a69c15be1a8d09017d3b7338fddf4ea2b955
fcy <807715333@qq.com>
2019-12-06 00:06:26 +0800

    '20191206'

---
+++ Algorithm/src/main/java/LeetCode/Recursion/longestUnivaluePath.scala
@@ -0,0 +1,11 @@
+package LeetCode.Recursion
+class TreeNode(var _value: Int) {
+     var value: Int = _value
+     var left: TreeNode = null
+     var right: TreeNode = null
+   }
+object longestUnivaluePath {
+  def longestUnivaluePath(root: TreeNode): Int = {
+
+  }
+}

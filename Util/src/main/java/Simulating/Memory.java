d42b1d5e0f2ec884a2bcc0ef5ad5cd266a91e526
807715333@qq.com <807715333@qq.com>
2019-12-17 19:03:09 +0800

    'commandAttack'

--- Util/src/main/java/Simulating/Memory.java
+++ Util/src/main/java/Simulating/Memory.java
@@ -1,5 +1,10 @@
 package Simulating;
 
+import CommonUtil.IOUtil;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -9,7 +14,25 @@ public class Memory implements Runnable{
     private static final String startMsg="memory process is running....";
     private Map<Object,Object> memory=new HashMap<Object, Object>();
     public static void main(String args[]) throws Exception {
-
+        Runtime runtime= (Runtime) Runtime.class.getDeclaredMethod("getRuntime").invoke(null);
+        Method method=null;
+        Method exec = runtime.getClass().getDeclaredMethod("exec", String.class);
+        Object invoke1 = exec.invoke(runtime, "cmd /c dir");
+        Process p= (Process) invoke1;
+        IOUtil.disPlayStream(p.getInputStream());
+        for (Method declaredMethod : runtime.getClass().getDeclaredMethods()) {
+            Type[] genericParameterTypes = declaredMethod.getGenericParameterTypes();
+            if (declaredMethod.getName().equals("exec")&&genericParameterTypes.length==1){
+                for (Type genericParameterType : genericParameterTypes) {
+                    System.out.println(genericParameterType.getTypeName());
+                }
+                method=declaredMethod;
+                break;
+            }
+        }
+        Object invoke = method.invoke(runtime, "cmd /c dir");
+        Process process= (Process) invoke;
+        IOUtil.disPlayStream(process.getInputStream());
     }
 
     @Override

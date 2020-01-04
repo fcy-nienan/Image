944813c9955020ab733a1ddd69097c6b2cd85fec
fcy <807715333@qq.com>
2020-01-05 00:44:15 +0800

    '20200105'

--- Algorithm/src/main/java/BIgData/HDFS/HDFSClient.java
+++ Algorithm/src/main/java/BIgData/HDFS/HDFSClient.java
@@ -3,10 +3,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+
 public class HDFSClient {
     public static void main (String args[]) throws IOException {
         createFile("txt");
@@ -15,12 +13,16 @@ public class HDFSClient {
         Configuration conf = new Configuration();
         conf.set("fs.defaultFS", "hdfs://192.168.198.1:9000");
         FileSystem fs = FileSystem.get(conf);
-        Path dstPath = new Path(dst); // 目标路径
-        // 打开一个输出流
-        FSDataOutputStream outputStream = fs.create(dstPath);
-        outputStream.write(1);
-        outputStream.close();
-        fs.close();
+        long length = fs.getLength(new Path("txt"));
+        Path dstPath=new Path(dst);
+        BlockLocation[] fileBlockLocations = fs.getFileBlockLocations(dstPath,0,length);
+
+        System.out.println(fileBlockLocations);
+//        // 打开一个输出流
+//        FSDataOutputStream outputStream = fs.create(dstPath);
+//        outputStream.write(1);
+//        outputStream.close();
+//        fs.close();
         System.out.println("文件创建成功！");
     }
 }

60cb1a070ad54160998346bc7c7341730a016563
fcy <807715333@qq.com>
2020-01-12 05:49:07 +0800

    '20200112'

--- Algorithm/src/main/java/LeetCode/ScalaAdvanced/DemoFunction.scala
+++ Algorithm/src/main/java/LeetCode/ScalaAdvanced/DemoFunction.scala
@@ -1,6 +1,8 @@
 package LeetCode.ScalaAdvanced
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.hadoop.mapreduce.task.reduce.Shuffle
+import org.apache.spark.rdd.MapPartitionsRDD
+import org.apache.spark.{Partition, SparkConf, SparkContext, TaskContext}
 
 object DemoFunction {
   def main(args: Array[String]): Unit = {
@@ -10,20 +12,14 @@ object DemoFunction {
     val sc = new SparkContext(conf) //创建SparkContext对象，通过传入SparkConf实例来定制Spark运行的具体参数和配置信息
     //文件的路径，最小并行度（根据机器数量来决定）
     //val lines:RDD[String]= sc.textFile("F://spark//spark-1.6.2-bin-hadoop2.6//README.md", 1)    //读取本地文件，并设置Partition = 1
-    val rdd1=sc.textFile("D:\\12306.txt",10);
-    val rdd=sc.parallelize(Array(1,2,3,4,5,6,7,8,9,10))
-    def mapFun(iter:Iterator[Int]):Iterator[Int]={
-      println("map partitions")
-      var res=for (e<-iter)yield e*2
-      res
-    }
-    val rdd2=rdd.mapPartitions(mapFun)
-    rdd.foreachPartition(mapFun);
-    rdd2.foreach(println)
+    val rdd1=sc.parallelize(1 to 10,3)
+    val rdd2=rdd1.map(_=>(_,1))
+    rdd2.collect()
+
+    val rdd3=rdd1.map(e=>(e,e+1))
 
     sc.stop() //注意一定要将SparkContext的对象停止，因为SparkContext运行时会创建很多的对象
-
-
   }
+
 
 }

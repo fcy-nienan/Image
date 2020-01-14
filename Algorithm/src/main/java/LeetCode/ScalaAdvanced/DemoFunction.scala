726046bbc35cef4c5d20697f77c5d53db45d6d4b
fcy <807715333@qq.com>
2020-01-15 03:54:43 +0800

    '20200115'

--- Algorithm/src/main/java/LeetCode/ScalaAdvanced/DemoFunction.scala
+++ Algorithm/src/main/java/LeetCode/ScalaAdvanced/DemoFunction.scala
@@ -9,10 +9,6 @@ import scala.collection.mutable
 
 object DemoFunction {
   def main(args: Array[String]): Unit = {
-    val set=new mutable.HashSet[String]
-    set+="df"
-
-
     val conf = new SparkConf()
     conf.setAppName("wordCountLocal") //设置应用程序的名称，在程序运行的监控界面可以看到名称
     conf.setMaster("local") //此时程序在本地运行，无需安装Spark的任何集群
@@ -20,11 +16,8 @@ object DemoFunction {
     //文件的路径，最小并行度（根据机器数量来决定）
     //val lines:RDD[String]= sc.textFile("F://spark//spark-1.6.2-bin-hadoop2.6//README.md", 1)    //读取本地文件，并设置Partition = 1
     val rdd1=sc.parallelize(1 to 10,3)
-    val rdd2=rdd1.map(_=>(_,1))
-    rdd2.collect()
-
-    val rdd3=rdd1.map(e=>(e,e+1))
-
+    val l = rdd1.map(e => (e, 1)).reduceByKey((e1, e2) => e1 + e2).count()
+    println(l)
     sc.stop() //注意一定要将SparkContext的对象停止，因为SparkContext运行时会创建很多的对象
   }
 

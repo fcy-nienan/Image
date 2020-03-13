7cdeb5756e1dcedc834eadeafc4f04c83d6f3853
807715333@qq.com <807715333@qq.com>
2020-03-13 18:04:35 +0800

    't'

---
+++ Java/src/main/java/Advanced/APT/APTProcessor.java
@@ -0,0 +1,47 @@
+package Advanced.APT;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+@SupportedAnnotationTypes("Advanced.APT.Getter")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class APTProcessor extends AbstractProcessor {
+    @Override
+    public Set<String> getSupportedOptions() {
+        return super.getSupportedOptions();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return super.getSupportedSourceVersion();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override
+    public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
+        return super.getCompletions(element, annotation, member, userText);
+    }
+
+    @Override
+    protected synchronized boolean isInitialized() {
+        return super.isInitialized();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
---
+++ Java/src/main/java/Advanced/APT/Student.java
@@ -0,0 +1,8 @@
+package Advanced.APT;
+
+import java.util.logging.Logger;
+@Getter
+public class Student {
+    private String username;
+    private String password;
+}

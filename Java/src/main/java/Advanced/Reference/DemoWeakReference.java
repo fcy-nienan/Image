19f92dd8ec083b43c1e801b6c903b6614261bad8
807715333 <807715333@qq.com>
2020-04-28 00:30:05 +0800

    'reference'

--- Java/src/main/java/Advanced/Reference/DemoWeakReference.java
+++ Java/src/main/java/Advanced/Reference/DemoWeakReference.java
@@ -1,10 +1,19 @@
 package Advanced.Reference;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @descripiton:
@@ -21,14 +30,56 @@ import java.lang.ref.WeakReference;
  *
  *
  */
+@Data
+@NoArgsConstructor
+class User{
+    private int[] array=new int[102400];
+    private int id;
+    private String username;
+    private String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
 @Slf4j
 public class DemoWeakReference {
-    public static void main(String[] args) throws InterruptedException {
-        ReferenceQueue queue=new ReferenceQueue();
-        ReferencedObject object=new ReferencedObject();
-        WeakReference reference=new WeakReference(object,queue);
-        checkReferenceQueueThread thread=new checkReferenceQueueThread(queue);
-        thread.start();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        List<Reference<Object>> list=new ArrayList();
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    list.add(new SoftReference(new User(0,"1","df")));
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t.start();
+
+        while (true){
+            System.out.println("start display:"+list.size());
+            for (int i=0;i<list.size();i++){
+                System.out.println(list.get(i).get());
+            }
+            Thread.sleep(3000);
+            System.gc();
+        }
 //        int i=0;
 //        long start=System.currentTimeMillis();
 //        while (true){
@@ -45,7 +96,6 @@ public class DemoWeakReference {
 //                break;
 //            }
 //        }
-        Thread.sleep(100000000);
 
     }
 
